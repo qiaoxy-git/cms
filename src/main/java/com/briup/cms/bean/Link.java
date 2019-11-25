43bc3c5ba6df828dc48c871cff06e7291acc480f
qiaoxy <909542849@qq.com>
2019-11-25 15:21:49 +0800

    cms

---
+++ src/main/java/com/briup/cms/bean/Link.java
@@ -0,0 +1,40 @@
+package com.briup.cms.bean;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cms_link")
+public class Link {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private String url;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
